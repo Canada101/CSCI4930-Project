c143cb1bc1719012736e7ead5241f2eb3c460cc4
Canada101 <sam.mercier@slu.edu>
2025-04-04 12:52:35 -0500

    updated queuing

--- prog.py
+++ prog.py
@@ -7,7 +7,6 @@ from tqdm import tqdm
 SIMULATION_LIMIT = 1_000_000
 TRANSMISSION_TIME = 2
 LINK_AVAILABILITY = 0.9
-MAX_QUEUE_LENGTH = 100
 GEN, TRANS, RECEIVE = 'GEN', 'TRANS', 'RECEIVE'
 
 class Event:
@@ -27,7 +26,6 @@ class Packet:
         self.path = path
         self.current_hop = 0
         self.rerouted = False
-        self.retry_count = 0
 
 class NetworkSimulator:
     def __init__(self, G, lam, ct, pbar):
@@ -63,12 +61,12 @@ class NetworkSimulator:
         return len(self.queues[node].get(dst, [])) >= self.congestion_threshold
 
     def enqueue_packet(self, node, dst, packet):
-        if len(self.queues[node].get(dst, [])) >= MAX_QUEUE_LENGTH:
-            self.Ndrop += 1
-            self.Ncong += 1
-            return False
         self.queues[node].setdefault(dst, []).append(packet)
-        return True
+
+    def dequeue_packet(self, node, dst):
+        if self.queues[node].get(dst):
+            return self.queues[node][dst].pop(0)
+        return None
 
     def process_events(self):
         last_progress = 0
@@ -97,15 +95,14 @@ class NetworkSimulator:
         dst = random.choice([n for n in self.G.nodes if n != src])
 
         try:
-            direct_path = nx.shortest_path(self.G, src, dst, weight='weight')
+            path = nx.shortest_path(self.G, src, dst, weight='weight')
         except nx.NetworkXNoPath:
             return
 
-        packet = Packet(src, dst, direct_path)
-        if not self.enqueue_packet(src, dst, packet):
-            return
-
+        packet = Packet(src, dst, path)
+        self.enqueue_packet(src, dst, packet)
         self.schedule_event(Event(self.current_time, TRANS, src, packet))
+
         inter_arrival = np.random.exponential(1 / self.lambda_rate)
         self.schedule_event(Event(self.current_time + inter_arrival, GEN, src, None))
         self.Ngen += 1
@@ -117,40 +114,47 @@ class NetworkSimulator:
 
         path = packet.path
         hop = packet.current_hop
+
         if hop >= len(path) - 1:
             return
 
         u, v = path[hop], path[hop + 1]
 
+        # Remove packet from the queue when transmitting
+        self.dequeue_packet(node, dst)
+
         if self.is_congested(node, dst):
             self.Ncong += 1
-            packet.retry_count += 1
-            if packet.retry_count < 10:
-                self.schedule_event(Event(self.current_time + TRANSMISSION_TIME, TRANS, node, packet))
-            else:
-                self.Ndrop += 1
+            if not packet.rerouted:
+                try:
+                    alt_path = nx.shortest_path(self.G, node, dst, weight='weight')
+                    if alt_path != packet.path:
+                        packet.path = alt_path
+                        packet.current_hop = 0
+                        packet.rerouted = True
+                        self.enqueue_packet(node, dst, packet)
+                        self.schedule_event(Event(self.current_time + TRANSMISSION_TIME, TRANS, node, packet))
+                        return
+                except nx.NetworkXNoPath:
+                    pass
+            self.Ndrop += 1
             return
 
         if not self.is_link_available():
             self.Nfail += 1
             if not packet.rerouted:
-                mid = [n for n in self.G.nodes if n not in (packet.src, packet.dst)]
-                if mid:
-                    try:
-                        new_path = [packet.src, mid[0], packet.dst]
-                        packet.path = new_path
+                try:
+                    alt_path = nx.shortest_path(self.G, node, dst, weight='weight')
+                    if alt_path != packet.path:
+                        packet.path = alt_path
                         packet.current_hop = 0
                         packet.rerouted = True
-                        if self.enqueue_packet(packet.src, packet.dst, packet):
-                            self.schedule_event(Event(self.current_time + TRANSMISSION_TIME, TRANS, packet.src, packet))
+                        self.enqueue_packet(node, dst, packet)
+                        self.schedule_event(Event(self.current_time + TRANSMISSION_TIME, TRANS, node, packet))
                         return
-                    except:
-                        pass
-            packet.retry_count += 1
-            if packet.retry_count < 10:
-                self.schedule_event(Event(self.current_time + TRANSMISSION_TIME, TRANS, node, packet))
-            else:
-                self.Ndrop += 1
+                except nx.NetworkXNoPath:
+                    pass
+            self.Ndrop += 1
             return
 
         self.Ngood += 1
@@ -161,11 +165,12 @@ class NetworkSimulator:
         packet = event.packet
 
         packet.current_hop += 1
+
         if node == packet.dst:
             self.Nsucc += 1
         else:
-            if self.enqueue_packet(node, packet.dst, packet):
-                self.schedule_event(Event(self.current_time, TRANS, node, packet))
+            self.enqueue_packet(node, packet.dst, packet)
+            self.schedule_event(Event(self.current_time, TRANS, node, packet))
 
     def get_results(self):
         r_all = self.Nsucc / (self.Nsucc + self.Ndrop) if (self.Nsucc + self.Ndrop) > 0 else 0
@@ -183,10 +188,6 @@ def run_multiple_lambdas():
     ct = int(input("Enter congestion threshold (CT): "))
     G_base = create_network()
 
-    lambda_values = []
-    r_all_values = []
-    p_cong_values = []
-
     lam = 0.1
     while lam <= 1.0:
         G = G_base.copy()
@@ -195,10 +196,6 @@ def run_multiple_lambdas():
             sim.initialize()
             sim.process_events()
             r_all, p_cong = sim.get_results()
-
-        lambda_values.append(lam)
-        r_all_values.append(r_all)
-        p_cong_values.append(p_cong)
 
         print(f"Lambda: {lam:.2f}, R_all: {r_all:.4f}, P_cong: {p_cong:.4f}")
 
